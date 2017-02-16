181f7cf2be1ba6778c0ec5005b6c5ee7d1596595
mjasantome <michaeljay.santome@gmail.com>
2017-02-16 21:52:55 +0800

    Review

--- Car/urls.py
+++ Car/urls.py
@@ -4,7 +4,12 @@ from Car import views
 app_name = 'Car'
 
 urlpatterns = [
-        
-    url(r'^$', views.cars, name='cars'),
-    url(r'^car/(?P<slug>[-\w]+)/$', views.car, name='cars'),
+    
+    url(r'^$', views.index, name='index'),
+    url(r'^cars/$', views.cars, name='cars'),
+    url(r'^motorcycles/$', views.motorcyles, name='motorcycles'),
+    url(r'^vehicles/$', views.vehicles, name='vehicles'),
+    url(r'^car/(?P<slug>[-\w]+)/$', views.car_detail, name='cars'),
+    url(r'^motorcycle/(?P<slug>[-\w]+)/$', views.motorcycle_detail, name='motorcycles'),
+    url(r'^vehicle/(?P<slug>[-\w]+)/$', views.vehicle_detail, name='vehicles'),
     ]
